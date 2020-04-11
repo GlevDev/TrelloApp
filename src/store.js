2cc4ed26ae40fe9234abb461df565927c6db4474
GlevDev <garth@glev.fr>
2020-04-11 22:50:40 +0200

    add drag and drop

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import defaultBoard from "./default-board";
-import { saveStatePlugin, uuid } from "./utils";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import defaultBoard from './default-board'
+import { saveStatePlugin, uuid } from './utils'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
-const board = JSON.parse(localStorage.getItem("board")) || defaultBoard;
+const board = JSON.parse(localStorage.getItem('board')) || defaultBoard
 
 export default new Vuex.Store({
   plugins: [saveStatePlugin],
@@ -13,28 +13,38 @@ export default new Vuex.Store({
     board
   },
   getters: {
-    getTask(state) {
+    getTask (state) {
       return id => {
         for (const column of state.board.columns) {
           for (const task of column.tasks) {
             if (task.id === id) {
-              return task;
+              return task
             }
           }
         }
-      };
+      }
     }
   },
   mutations: {
-    CREATE_TASK (state, {tasks, name}) {
+    CREATE_TASK (state, { tasks, name }) {
       tasks.push({
         name,
         id: uuid(),
         description: ''
       })
     },
-    UPDATE_TASK (state, {task, key, value}) {
-      Vue.set(task, key, value)
+    UPDATE_TASK (state, { task, key, value }) {
+      task[key] = value
+      // Vue.set(task, key, value)
+    },
+    MOVE_TASK (state, { fromTasks, toTasks, fromTaskIndex, toTaskIndex }) {
+      const taskToMove = fromTasks.splice(fromTaskIndex, 1)[0]
+      toTasks.splice(toTaskIndex, 0, taskToMove)
+    },
+    MOVE_COLUMN (state, { fromColumnIndex, toColumnIndex }) {
+      const columnList = state.board.columns
+      const columnToMove = columnList.splice(fromColumnIndex, 1)[0]
+      columnList.splice(toColumnIndex, 0, columnToMove)
     }
   }
-});
+})
