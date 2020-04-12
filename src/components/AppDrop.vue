6d1986a0cc81ce83c6015c1e89cee86df102abbd
GlevDev <garth@glev.fr>
2020-04-12 22:29:37 +0200

    finish refactoring

---
+++ src/components/AppDrop.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    @drop="onDrop"
+    @dragover.prevent
+    @dragenter.prevent
+  >
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onDrop (e) {
+      e.stopPropagation()
+      const transferData = JSON.parse(e.dataTransfer.getData('payload'))
+      this.$emit('drop', transferData)
+    }
+  }
+}
+</script>
